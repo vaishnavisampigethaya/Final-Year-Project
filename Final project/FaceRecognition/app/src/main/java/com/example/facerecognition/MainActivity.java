9baf3c742ebd9012b6fab24f6abc17b548220d52
vaishnavisampigethya <vaishnavisampigethya@gmail.com>
2021-05-11 14:51:38 +0530

    fifth commit

--- Final project/FaceRecognition/app/src/main/java/com/example/facerecognition/MainActivity.java
+++ Final project/FaceRecognition/app/src/main/java/com/example/facerecognition/MainActivity.java
@@ -1,27 +1,96 @@
 package com.example.facerecognition;
 
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-import com.chaquo.python.PyObject;
-import com.chaquo.python.Python;
-import com.chaquo.python.android.AndroidPlatform;
 
-public class MainActivity extends AppCompatActivity {
-    TextView textview;
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    //TextView textview;
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textview=(TextView)findViewById(R.id.textview);
+      /*  textview= findViewById(R.id.textview);
         if (! Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
         }
         Python py=Python.getInstance();
-        PyObject pyobj=py.getModule("myfile");
+        PyObject pyobj=py.getModule("facerec");
         PyObject obj=pyobj.callAttr("main");
-        textview.setText(obj.toString());
+        textview.setText(obj.toString());*/
+        cameraBridgeViewBase=(JavaCameraView)findViewById(R.id.CameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+        baseLoaderCallback=new BaseLoaderCallback(null) {
+            @Override
+            public void onManagerConnected(int status) {
+                super.onManagerConnected(status);
+                if (status == BaseLoaderCallback.SUCCESS) {
+                    cameraBridgeViewBase.enableView();
+                } else {
+                    super.onManagerConnected(status);
+                }
+            }
+        };
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat frame = inputFrame.rgba();
+
+        return frame;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()){
+            Toast.makeText(getApplicationContext(),"There's a problem, yo!", Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        super.onPause();
+        if(cameraBridgeViewBase!=null){
+
+            cameraBridgeViewBase.disableView();
+        }
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
+        }
     }
 }
